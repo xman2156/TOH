64b65601581390c1fc7b900c6321a540de8a3270
xman2156 <34029777+xman2156@users.noreply.github.com>
2021-10-07 16:52:38 -0500

    Lots of changes

--- modal.js
+++ modal.js
@@ -2,37 +2,47 @@ var spacer = document.getElementsByClassName("spacer");
 var modal = document.getElementById("info");
 var btn = document.getElementById("infoBtn");
 var closeBtn = document.getElementsByClassName("close")[0];
-var body = document.body;
+var body = document.body.style;
 var i;
 
 
 function dim() {
-  body.style.backgroundSize = "cover";
-  body.style.backgroundRepeat = "no-repeat";
-  body.style.backgroundPosition = "center";
-  body.style.backgroundAttachment = "fixed";
-  body.style.color = "#006680";
+  body.background = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('background.webp')";
+  body.backgroundSize = "cover";
+  body.backgroundRepeat = "no-repeat";
+  body.backgroundPosition = "center";
+  body.backgroundAttachment = "fixed";
+  body.color = "#006680";
   for (i=0; i < spacer.length; i++) {
     spacer[i].style.background = "#006680";
   }
 }
 
+function bright() {
+  body.background = "url('background.webp')";
+  body.backgroundSize = "cover";
+  body.backgroundRepeat = "no-repeat";
+  body.backgroundPosition = "center";
+  body.backgroundAttachment = "fixed";
+  body.color = "#3cd8ff";
+  for (i=0; i < spacer.length; i++) {
+    spacer[i].style.background = "#3cd8ff";
+  }
+}
+
 btn.onclick = function() {
   modal.style.display = "block";
-  body.style.background = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('background.webp')";
   dim()
 }
 
 closeBtn.onclick = function() {
   modal.style.display = "none";
-  document.body.style.background = "url('background.webp')";
-  
+  bright()
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    document.body.style.background = "url('background.webp')";
-    dim()
+    bright()
   }
 }
---
+++ youtube.js
@@ -0,0 +1,28 @@
+( function() {
+
+	var youtube = document.getElementById("youtube");
+	
+
+		
+		var source = "https://img.youtube.com/vi/j0Xk_Wa6Ync/sddefault.jpg";
+		
+		var image = new Image();
+				image.src = source;
+				image.addEventListener( "load", function() {
+					youtube.appendChild( image );
+				} );
+		
+				youtube.addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+
+							iframe.setAttribute( "frameborder", "0" );
+							iframe.setAttribute( "allowfullscreen", "" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/j0Xk_Wa6Ync?autoplay=1" );
+
+							this.innerHTML = "";
+							this.appendChild( iframe );
+				} );	
+
+	
+} )();
