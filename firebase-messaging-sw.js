5252678fc9ccbaddba7b806eff9bfe03dbecec1c
xman2156 <34029777+xman2156@users.noreply.github.com>
2022-07-21 15:09:36 -0500

    test notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js");
+
+firebase.initializeApp({
+  messagingSenderId: "179906695397"
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(payload => {
+  const notification = JSON.parse(payload.data.notification);
+  const notificationTitle = notification.title;
+  const notificationOptions = {
+    body: notification.body
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
