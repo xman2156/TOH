d3da61d9ca5a3ec5c2f2dbc7a06ce81c006d88c3
xman2156 <34029777+xman2156@users.noreply.github.com>
2023-04-15 10:04:38 -0500

    Fix for final episode

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,60 +1,7 @@
-//Automatic episode change
-var episodeList = [
-  ['the end of The Owl House', '', new Date("2023-04-08T20:30:00-05:00"),]
-];
-var startDate = new Date("2023-04-08T20:30:00-05:00");
-var today = Date.now();
-var weeksPassed = 0; //Math.floor((today - Date.parse(startDate))/(24*3600*1000*7)); //comment out equation when on hiatus
-var latestRelease = episodeList[weeksPassed][2];
-var nextRelease = episodeList[weeksPassed][3];
-document.getElementById("previousEpisode").innerHTML = episodeList[weeksPassed][0];
-document.getElementById("nextEpisode").innerHTML = episodeList[weeksPassed][1];
-
+var latestRelease = new Date("2023-04-08T20:30:00-05:00");
 var oneDay = 24*60*60*1000;
 var mode = 0; //DD:HH:MM:SS mode is default
 var lastHiatusMention = null;
-
-function GetThen(yourUrl, onload){
-  var Httpreq = new XMLHttpRequest();
-  Httpreq.open("GET",yourUrl,true);
-  Httpreq.onload = function() {
-    if (Httpreq.readyState === Httpreq.DONE && Httpreq.status === 200) {
-      onload(Httpreq.responseText);
-    }
-  };
-  Httpreq.send(null);
-}
-
-//Initially loads the last 100 posts on subreddit
-function requestSubredditData(after = null) {
-  var url = 'https://www.reddit.com/r/TheOwlHouse/new.json?limit=100';
-  GetThen(after ? url + '&after=' + after : url, checkSubreddit);
-}
-		
-//looks at the loaded posts, this runs four times every half-second
-function checkSubreddit(response){
-  var subbredditJSON = JSON.parse(response);
-  var lastHiatusMentionThisCheck;
-  //list of words that counts as a mention of the hiatus
-  var keywords = ["hiatus"];
-  for(var i = 0; i < 100; i++){
-    for(var j = 0; j < keywords.length; j++){
-      //checks only post titles and post content if self-post
-      if(subbredditJSON.data.children[i].data.selftext.toLowerCase().includes(keywords[j]) == true || subbredditJSON.data.children[i].data.title.toLowerCase().includes(keywords[j]) == true){
-        lastHiatusMentionThisCheck = new Date(subbredditJSON.data.children[i].data.created_utc * 1000);
-        document.getElementById("hiatusLink").href = "https://reddit.com" + subbredditJSON.data.children[i].data.permalink;
-        i = 100;
-      };
-    };
-  };
-  //loads the next 100 if hiatus is not mentioned then runs the function again
-  if (lastHiatusMentionThisCheck == null) {
-    requestSubredditData(subbredditJSON.data.after);
-  }
-  else {
-    lastHiatusMention = lastHiatusMentionThisCheck;
-  }
-};
 
 function switchMode(){
   if(mode == 0){
@@ -126,46 +73,9 @@ var hiatusList = [
   ["Yesterday's Lie",'Follies at the Coven Day Parade','Aug 14 2021','Mar 19 2022',217,''],
   ["King's Tide",'Thanks to Them','May 28 2022','Oct 15 2022',140,''],
   ['Thanks to Them','For The Future','Oct 15 2022','Jan 21 2023',98,''],
-  ['For The Future','Watching and Dreaming','Jan 21 2023','Apr 8 2023',,''],
+  ['For The Future','Watching and Dreaming','Jan 21 2023','Apr 8 2023',77,''],
   ['Watching and Dreaming','','Apr 8 2023','','','The end of The Owl House']
 ];
-
-function hiatusRankCheck(){
-  var diffDays = timer("up", latestRelease, "count");
-  var hiatusRank = 0;
-  var nextHiatusLength = hiatusList[2][4]; //reference to the longest hiatus
-  for(var i = 1; i < hiatusList.length; i++){
-    if(hiatusList[i][4] > diffDays){
-      hiatusRank += 1;
-      if(hiatusList[i][4] < nextHiatusLength){
-        nextHiatusLength = hiatusList[i][4];
-      }
-    }
-  }
-  var suffix;
-  if(hiatusRank % 10 == 1 && hiatusRank != 11){
-    suffix = "st";
-  }
-  else if(hiatusRank % 10 == 2 && hiatusRank != 12){
-    suffix = "nd";
-  }
-  else if(hiatusRank % 10 == 3 && hiatusRank != 13){
-    suffix = "rd";
-  }
-  else if(hiatusRank == 0){
-    suffix = "The";
-  }
-  else suffix = "th";
-  if(hiatusRank > 0){
-    document.getElementById("hiatusRank").innerHTML =  hiatusRank + suffix;
-  }
-  else{
-    document.getElementById("hiatusRank").innerHTML =  suffix;
-  }
-  document.getElementById("nextHiatusLength").innerHTML =  nextHiatusLength;
-  var nextHiatusLengthDate = new Date(latestRelease.getTime() + (nextHiatusLength * 86400000));
-  return nextHiatusLengthDate;
-}
 
 //makes an HTML table from the array
 function createTable(array) {
@@ -188,6 +98,3 @@ function createTable(array) {
 window.setInterval(function(){
   timer("up", latestRelease, "count");
 }, 250);
-
-//every 30 seconds, the most recent 100 posts on the subreddit are loaded up again in case there has been a new post that mentions hiatus
-window.setInterval(requestSubredditData, 30000);
